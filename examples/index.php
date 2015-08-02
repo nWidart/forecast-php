4cbcac6e37f9e56291b993b22d3f8f502ca09861
Nicolas Widart <n.widart@gmail.com>
2015-08-02 16:20:15 +0200

    Uncomment code

--- examples/index.php
+++ examples/index.php
@@ -7,9 +7,9 @@ $forecastPhp = new \Nwidart\ForecastPhp\Forecast('3da43a66de8ca36593e0f44324f496
 $info = $forecastPhp->get('40.7324296', '-73.9977264');
 
 // Fetch weather at a given time
-// $info = $forecastPhp->get('40.7324296', '-73.9977264', '2013-05-06T12:00:00-0400');
+$info = $forecastPhp->get('40.7324296', '-73.9977264', '2013-05-06T12:00:00-0400');
 
 // Add options to the request
-// $info = $forecastPhp->setOptions(['units' => 'si',])->get('40.7324296', '-73.9977264');
+$info = $forecastPhp->setOptions(['units' => 'si',])->get('40.7324296', '-73.9977264');
 
 var_dump($info);
