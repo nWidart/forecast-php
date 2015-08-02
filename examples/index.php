4f00a83c922877288a66ea766e49e2396f30f808
Nicolas Widart <n.widart@gmail.com>
2015-08-02 16:20:29 +0200

    Uncomment code

--- examples/index.php
+++ examples/index.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$forecastPhp = new \Nwidart\ForecastPhp\Forecast('3da43a66de8ca36593e0f44324f49607');
+$forecastPhp = new \Nwidart\ForecastPhp\Forecast('your_api_key');
 
 $info = $forecastPhp->get('40.7324296', '-73.9977264');
 
